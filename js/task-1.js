4ba07a90302c8a673183b8765ee6b73f6810e826
Olena Tykhonovych <olena.tykhonovych@gmail.com>
2020-08-31 23:32:48 +0300

    Готова 1 задача

--- js/task-1.js
+++ js/task-1.js
@@ -3,12 +3,17 @@
 //Нумерация должна начинаться с 1. К примеру для первого элемента массива['Mango', 'Poly', 'Ajax'] с индексом 0 будет выведено '1 - Mango', а для индекса 2 выведет '3 - Ajax'.
 
 const logItems = function (array) {
-  // твой код
+  'use strict';
+
+  let result = '';
+
+  for (let i = 0; i < array.length; i += 1) {
+    result += `${i + 1} - ${array[i]}\n`;
+  }
+
+  return result;
 };
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
-logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+console.log(logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
 
-logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+console.log(logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]));
