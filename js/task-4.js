fa1d6f17944768baf83622e1b71f1833e4ef4da5
Olena Tykhonovych <olena.tykhonovych@gmail.com>
2020-09-04 22:18:37 +0300

    Готова 4 задача

--- js/task-4.js
+++ js/task-4.js
@@ -3,13 +3,20 @@
 // Если длина строки не превышает 40 символов, функция возвращает ее в исходном виде.
 // Если длина больше 40 символов, то функция обрезает строку до 40 - ка символов и добавляет в конец строки троеточие '...', после чего возвращает укороченную версию.
 
-const formatString = function (string) {
-  // твой код
-};
+function formatString(string, maxLength = 40) {
+  const stringSplit = string.split('');
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
+  if (stringSplit.length > maxLength) {
+    stringSplit.length = maxLength;
+
+    return stringSplit.join('') + '...';
+  } else if (
+    (stringSplit.length = maxLength || stringSplit.length < maxLength)
+  ) {
+    return stringSplit.join('');
+  }
+}
+
 console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
 // вернется оригинальная строка
 
