ef4b960e92222b0c152f8afb5b2aa7797b34f6a3
Olena Tykhonovych <olena.tykhonovych@gmail.com>
2020-09-04 22:23:16 +0300

    Готова 5 задача

--- js/task-5.js
+++ js/task-5.js
@@ -2,12 +2,18 @@
 // Функция проверяет ее на содержание слов spam и sale.Если нашли зарещенное слово то функция возвращает true, если запрещенных слов нет функция возвращает false.Слова в строке могут быть в произвольном регистре.
 
 const checkForSpam = function (message) {
-  // твой код
+  'use strict';
+
+  if (
+    message.toLowerCase().includes('spam') ||
+    message.toLowerCase().includes('sale')
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
 console.log(checkForSpam('Latest technology news')); // false
 
 console.log(checkForSpam('JavaScript weekly newsletter')); // false
