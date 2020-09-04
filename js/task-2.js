79e060cce23dbe07fdce1defd7aa6370ce30c15c
Olena Tykhonovych <olena.tykhonovych@gmail.com>
2020-09-04 22:03:29 +0300

    Готова 2 задача

--- js/task-2.js
+++ js/task-2.js
@@ -1,13 +1,9 @@
 // Напиши скрипт подсчета стоимости гравировки украшений.
 // Для этого создай функцию calculateEngravingPrice(message, pricePerWord) принимающую строку(в строке будут только слова и пробелы) и цену гравировки одного слова, и возвращающую цену гравировки всех слов в строке.
 
-const calculateEngravingPrice = function (message, pricePerWord) {
-  // твой код
-};
+const calculateEngravingPrice = (message = '', pricePerWord = 0) =>
+  message.split(' ').length * pricePerWord;
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
 console.log(
   calculateEngravingPrice(
     'Proin sociis natoque et magnis parturient montes mus',
